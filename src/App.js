81e2c89cd3903dd69d4b93df50566cde457ff112
Anshu Kumar <anshuzxcv@gmail.com>
2021-03-14 09:34:01 +0530

    Adding basic Routing feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, {Component} from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Page2 from './pages/page2/Page2'
 import Page1 from './pages/page1/Page1'
-function App() {
-  return (
-    <Page1/>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={}
+  }
+  render(){
+    return(
+      <div>
+        <Switch>
+          <Route exact path='/page1' component={Page1} />
+          <Route exact path='/page2' component={Page2} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/pages/page2/Page2.jsx
+++ src/pages/page2/Page2.jsx
@@ -6,27 +6,26 @@ import FilterMenu from '../../components/filterMenu/FilterMenu';
 
 function Page2(){
     return(
-            <div className="main-div">
-                <div className="center-div">
-                <Heading location="Patna, Bihar, India"></Heading>
-                <FilterMenu></FilterMenu>
-                 <Scrollbars>
-                 <ListItem
-                 percent = {20}
-                 name = {"Energy Park Parking"}
-                 address = {"BSEB Colony Road, Patna"}
-                 />
+        <div className="main-div">
+            <div className="center-div">
+            <Heading location="Patna, Bihar, India"></Heading>
+            <FilterMenu></FilterMenu>
+                <Scrollbars>
+                <ListItem
+                percent = {20}
+                name = {"Energy Park Parking"}
+                address = {"BSEB Colony Road, Patna"}
+                />
 
-                 <ListItem
-                 percent = {50}
-                 name = {"Energy Park Parking"}
-                 address = {"BSEB Colony Road, Patna"}
-                 />
+                <ListItem
+                percent = {50}
+                name = {"Energy Park Parking"}
+                address = {"BSEB Colony Road, Patna"}
+                />
 
-
-                 </Scrollbars>
-                </div>
-            </div>  
+                </Scrollbars>
+            </div>
+        </div>  
     );
 }
 export default Page2;
